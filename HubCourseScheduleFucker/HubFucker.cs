dab1f68c6d19f5cde5f4f688d3efaa9d5f2bb4bf
李博修 <liboxiu@dreamofalice.club>
2020-11-17 16:19:18 +0800

    done

--- HubCourseScheduleFucker/HubFucker.cs
+++ HubCourseScheduleFucker/HubFucker.cs
@@ -28,25 +28,13 @@ namespace HubCourseScheduleFucker
         HttpClient client;
         bool first = true;
         HttpClientHandler handler;
-        MyRequester requester;
-        IDocument newdoc;
         Engine eng;
 
         public HubFucker()
         {
-            var a = new DefaultHttpRequester();
-            handler = new HttpClientHandler() { MaxAutomaticRedirections = 100}; //{ UseCookies = false };
+            handler = new HttpClientHandler() { MaxAutomaticRedirections = 100};
             client = new HttpClient(handler);
-            requester = new MyRequester();
             var config = Configuration.Default;
-                //.WithRequester(requester)
-                //.WithDefaultLoader(new LoaderOptions
-                //{
-                //    IsResourceLoadingEnabled = true,
-                //})
-                //.WithCookies().WithJs().WithEventLoop().WithMetaRefresh();
-
-            //Create a new context for evaluating webpages with the given config
             context = BrowsingContext.New(config);
         }
         public async ValueTask<Stream> GetValidationCodeGifAsync()
@@ -63,7 +51,6 @@ namespace HubCourseScheduleFucker
                     document = await context.OpenAsync(ve => ve.Content(html));
                     
                     //document = await context.OpenAsync("http://hub.m.hust.edu.cn/kcb/index.jsp?v=1");
-                    await document.WaitForReadyAsync();
                 }
                 catch (Exception e)
                 {
@@ -72,9 +59,7 @@ namespace HubCourseScheduleFucker
                 }
                 first = false;
             }
-            //var c = context.GetCookie(new Url("https://pass.hust.edu.cn/cas/code"));
             var message = new HttpRequestMessage(System.Net.Http.HttpMethod.Get, "https://pass.hust.edu.cn/cas/code");
-            //message.Headers.Add("Cookie", c);
             var result = await client.SendAsync(message);
             result.EnsureSuccessStatusCode();
             return await result.Content.ReadAsStreamAsync();
@@ -84,37 +69,18 @@ namespace HubCourseScheduleFucker
             var lt = document.GetElementById("lt") as IHtmlInputElement;
             var enc = eng.GetValue("strEnc");
             var des = enc.Invoke($"{stuId + passwd + lt.Value}", "1", "2", "3").AsString();
-            //var form = document.GetElementById("loginForm") as IHtmlFormElement;
-            var o = new
-            {
-                code = code,
-                rsa = des,
-                ul = stuId.Length,
-                pl = passwd.Length,
-                lt = lt.Value,
-                execution = "e1s1",
-                _eventId = "submit"
-            };
             var passurl = new Uri("https://pass.hust.edu.cn/");
             var header = handler.CookieContainer.GetCookieHeader(passurl);
             var nc = $"cas_hash=; hust_cas_un={stuId}; {header}";
             handler.CookieContainer.SetCookies(passurl, nc);
-            //var jsession = header.Split(';')[1].Split('=')[1];
             var u = "http://hub.m.hust.edu.cn/kcb/index.jsp?v=1";
             var url = new Url($"https://pass.hust.edu.cn/cas/login?service={HttpUtility.UrlEncode(u)}");
-            var form = new MultipartFormDataContent();
-            form.Add(new StringContent(code), "code");
-            form.Add(new StringContent(des), "rsa");
-            form.Add(new StringContent($"{stuId.Length}"), "ul");
-            form.Add(new StringContent($"{passwd.Length}"), "pl");
-            form.Add(new StringContent($"{lt.Value}"), "lt");
-            form.Add(new StringContent("e1s2"), "execution");
-            form.Add(new StringContent("submit"), "_eventId");
-            form.Headers.ContentType = new MediaTypeHeaderValue("application/x-www-form-urlencoded");
-            
+            var content = new StringContent($"code={code}&rsa={des}&ul={stuId.Length}&pl={passwd.Length}&lt={lt.Value}&execution=e1s1&_eventId=submit");
+            content.Headers.ContentType = new MediaTypeHeaderValue("application/x-www-form-urlencoded");
+
 
             var msg = new HttpRequestMessage(System.Net.Http.HttpMethod.Post, url);
-            msg.Content = form;
+            msg.Content = content;
             msg.Headers.Add("Host", "pass.hust.edu.cn");
             msg.Headers.Add("Origin", "https://pass.hust.edu.cn");
             msg.Headers.Add("Sec-Fetch-Dest", "document");
@@ -125,18 +91,14 @@ namespace HubCourseScheduleFucker
             msg.Headers.UserAgent.ParseAdd("Mozilla/5.0 (Linux; Android 6.0; Nexus 5 Build/MRA58N) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/86.0.4240.111 Mobile Safari/537.36 Edg/86.0.622.56");
             msg.Headers.Accept.ParseAdd("text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9");
             msg.Headers.AcceptEncoding.ParseAdd("gzip, deflate, br");
-            msg.Headers.Add("Cookie", nc);
 
             msg.Headers.Referrer = new Uri("https://pass.hust.edu.cn/cas/login?service=http%3A%2F%2Fhub.m.hust.edu.cn%2Fkcb%2Findex.jsp%3Fv%3D1");
 
 
-            client = new HttpClient(new HttpClientHandler { UseCookies = false });
             var submitResult = await client.SendAsync(msg);
-            var rehtml = await submitResult.Content.ReadAsStringAsync();
-            var he = handler.CookieContainer.GetCookieHeader(new Url("http://hub.m.hust.edu.cn"));
-
-            //newdoc = await form.SubmitAsync(o);
-            await newdoc.WaitForReadyAsync();
+            
+            var fin = await client.GetAsync(submitResult.Headers.Location);
+            fin.EnsureSuccessStatusCode();
 
         }
         public async ValueTask<List<Lecture>> GetDailyLectureAsync(int week, DayOfWeek dayOfWeek)
@@ -148,7 +110,6 @@ namespace HubCourseScheduleFucker
             var req = $"http://hub.m.hust.edu.cn/kcb/todate/JsonCourse.action?sj={timeSlug}&zc={week}";
 
             var message = new HttpRequestMessage(System.Net.Http.HttpMethod.Get, req);
-            message.Headers.Add("Cookie", newdoc.Cookie);
             var result = await client.SendAsync(message);
             return await JsonSerializer.DeserializeAsync<List<Lecture>>(await result.Content.ReadAsStreamAsync());
         }
